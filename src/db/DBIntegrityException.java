2d2b6dacfab890ec89f7a70ed1c8c9f8b9b5b021
rafa9694 <rafa9694@gmail.com>
2021-10-29 20:53:21 -0300

    Adding database acess

---
+++ src/db/DBIntegrityException.java
@@ -0,0 +1,14 @@
+package db;
+
+public class DBIntegrityException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public DBIntegrityException(String msg) {
+		super(msg);
+	}
+
+}
